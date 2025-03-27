7bb81449e7db3042db3692b1d36e1c65d8b69447
Chelsea Edmonds <cledmonds94@gmail.com>
2025-03-27 22:29:54 +0000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'Isabelle Tutorials'
+copyright = 'Chelsea Edmonds'
+author = 'Chelsea Edmonds'
 release = '0.1'
 
 
